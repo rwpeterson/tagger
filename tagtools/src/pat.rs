ac87ab9697bbc8d4d8f613b89e42bf51265e8b16
Bob Peterson <bob@rwp.is>
2021-07-08 19:06:00 +0200

    tagtools::pat: add arbitrary pattern matching

--- tagtools/src/pat.rs
+++ tagtools/src/pat.rs
@@ -1,10 +1,10 @@
 //! Tools for analyzing patterns in time tag datasets
 
 use crate::Tag;
+use bit_iter::BitIter;
 use itertools::Itertools;
 use std::cmp;
-use std::collections::VecDeque;
-use std::vec::Vec;
+use std::collections::{HashMap, VecDeque};
 
 /// Count number of events in a given channel.
 pub fn singles(tags: &[Tag], ch: u8) -> usize {
@@ -87,6 +87,65 @@ pub fn coincidence(tags: &[Tag], ch_a: u8, ch_b: u8, win: i64, delay: i64) -> us
         }
     }
     return count;
+}
+
+/// Count arbitrary patterns at a fixed delay per channel
+pub fn patterns(tags: &[Tag], pats: &[u16], win: i64, delays: [i64; 16]) -> HashMap<u16, u64> {
+    // Time-related values are signed to avoid ubiquitous checking during casting,
+    // but the following need to be nonnegative:
+    assert!(win >= 0);
+    assert!(delays.iter().map(|d| !d.is_negative()).fold(true, |a, d| a & d));
+
+    // Delay, bin, and re-sort the tags
+    // After this point, tags are coincident if their times are identical
+    let t = tags.iter()
+        .map(|&t| Tag {
+            time: (t.time + delays[t.channel as usize - 1]) / win,
+            channel: t.channel
+        })
+        .sorted()
+        .collect::<Vec<_>>();
+
+    let mut tag_iter = t.iter().peekable();
+
+    let mut counts = HashMap::<u16, u64>::new();
+    for &pat in pats {
+        counts.insert(pat, 0);
+    }
+
+    // We expect to see ~N events per window for an N-fold pattern
+    let x = pats.iter().map(|&y| BitIter::from(y).count()).max().unwrap_or(1);
+    let mut buffer = VecDeque::<Tag>::with_capacity(2 * x);
+
+    // Seed the buffer with one tag
+    if let Some(&t) = tag_iter.next() {
+        buffer.push_back(t);
+    }
+
+    while !buffer.is_empty() {
+        if let Some(t0) = buffer.pop_front() {
+            // Fill buffer with tags at same time
+            buffer.extend(
+                tag_iter.peeking_take_while(|&&t| t.time == t0.time)
+            );
+            // Drain buffer into pattern mask to check against
+            let mask = buffer.drain(..).fold(0u16, |a, t| 1 << (t.channel - 1) | a);
+            for pat in pats {
+                if *pat & mask == *pat {
+                    if let Some(c) = counts.get_mut(pat) {
+                        *c += 1;
+                    }
+                }
+            }
+        }
+        // Don't leave buffer empty for the next loop
+        if buffer.is_empty() {
+            if let Some(&t) = tag_iter.next() {
+                buffer.push_back(t);
+            }
+        }
+    }
+    return counts;
 }
 
 /// Calculate the raw coincidence histogram between ch_a, ch_b in a given
