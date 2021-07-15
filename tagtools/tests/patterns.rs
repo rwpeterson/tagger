8a4ee1e63920065f0a6213bd0172b0870d67d0a4
Bob Peterson <bob@rwp.is>
2021-07-15 17:09:26 +0200

    Remove tests for unimpl functions

--- tagtools/tests/patterns.rs
+++ tagtools/tests/patterns.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use tagtools::pat;
 
 mod common;
@@ -9,22 +8,6 @@ fn singles() {
     assert_eq!(pat::singles(&tags, 3), 223_058);
     assert_eq!(pat::singles(&tags, 15), 251_662);
     assert_eq!(pat::singles(&tags, 16), 25_280);
-}
-
-#[test]
-fn pattern_singles() {
-    let tags = common::load_test_data();
-    let mut proofs = HashMap::<u16, u64>::new();
-    proofs.insert(1 << 2, 223_058);
-    proofs.insert(1 << 14, 251_662);
-    proofs.insert(1 << 15, 25_280);
-
-    let delays = [0; 16];
-    let pats = proofs.keys().cloned().collect::<Vec<_>>();
-    let hashmap = pat::patterns(&tags, &pats, 1, delays);
-    for (pat, cts) in proofs {
-        assert_eq!(hashmap.get(&pat).unwrap(), &cts);
-    }
 }
 
 #[test]
@@ -41,36 +24,6 @@ fn coincidence_histogram() {
         } else if i == 128 {
             continue;
         }
-        let histogram_x = (-64. + i as f64) * 0.15625; // in ns
-        assert!((proof.x - histogram_x).abs() < 1e-4);
-        assert_eq!(proof.y, histogram[i]);
-    }
-}
-
-#[test]
-fn pattern_coincidence_histogram() {
-    let tags = common::load_test_data();
-    let proof_histogram = common::load_coincidence_histogram();
-    let mut histogram = Vec::new();
-    let pat: u16 = (1 << 2) | (1 << 14);
-    for delay in -64..=64 {
-        let mut delays = [0; 16];
-        let d = delays.get_mut(14).unwrap();
-        *d = delay;
-        let hm = pat::patterns(&tags, &vec![pat], 1, delays);
-        let ct = hm.get(&pat).unwrap();
-        histogram.push(*ct);
-    }
-
-    assert_eq!(proof_histogram.len(), histogram.len());
-    for (i, proof) in proof_histogram.into_iter().enumerate() {
-        // skip the first and last bins since they are zero in the reference calculation
-        if i == 0 {
-            continue;
-        } else if i == 128 {
-            continue;
-        }
-        println!("{0}\t{1}\t{2}", -64 + i as i64, proof.y, histogram[i]);
         let histogram_x = (-64. + i as f64) * 0.15625; // in ns
         assert!((proof.x - histogram_x).abs() < 1e-4);
         assert_eq!(proof.y, histogram[i]);
