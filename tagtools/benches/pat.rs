e40f1392354691a85048bd42a1ecb33c96cb5d74
Bob Peterson <bob@rwp.is>
2021-12-13 14:02:10 +0100

    tagtools: consolidate benches for coincidences

--- tagtools/benches/pat.rs
+++ tagtools/benches/pat.rs
@@ -1,5 +1,5 @@
 #[allow(unused_imports)]
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use tagtools::pat;
 
@@ -15,30 +15,34 @@ fn singles(c: &mut Criterion) {
     });
 }
 
-fn coincidence_histogram_1(c: &mut Criterion) {
+fn coincidences(c: &mut Criterion) {
     let tags = common::load_test_data();
-
-    c.bench_function("coincidence_histogram_1", |b| {
-        b.iter(|| {
-            pat::coincidence(&tags, black_box(3), 15, 1, 26);
-        })
-    });
-}
-
-fn coincidence_intersection(c: &mut Criterion) {
-    let tags = common::load_test_data();
-
-    c.bench_function("coincidence_intersection", |b| {
-        b.iter(|| {
-            pat::coincidence_intersection(&tags, black_box(3), 15, 1, 26);
-        })
-    });
+    let mut group = c.benchmark_group("Coincidences");
+    for i in (-64..=64i64).step_by(8) {
+        group.bench_with_input(
+            BenchmarkId::new("histogram", i),
+            &i,
+            |b, i| {
+                b.iter(|| {
+                    pat::coincidence_histogram_1(&tags, black_box(3), 15, 1, *i);
+                });
+            }
+        );
+        group.bench_with_input(
+            BenchmarkId::new("intersection", i),
+            &i,
+            |b, i| {
+                b.iter(|| {
+                    pat::coincidence_intersection(&tags, black_box(3), 15, 1, *i);
+                });
+            }
+        );
+    }
 }
 
 criterion_group!(
     benches,
     singles,
-    coincidence_histogram_1,
-    coincidence_intersection
+    coincidences,
 );
 criterion_main!(benches);
