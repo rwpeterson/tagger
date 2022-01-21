151b4007e06d5f8603dfa1910a0daf2b25389f49
Bob Peterson <bob@rwp.is>
2022-01-21 14:18:34 +0100

    tagtools: refactor and modernize tcat and txt2tags
    This adds proper arg parsing, Windows compatibility (Rust cannot
    read/write non-UTF-8 bytes to stdio), and simplifies the organization
    of these utilities

--- tagtools/src/bin/tcat.rs
+++ tagtools/src/bin/tcat.rs
@@ -1,54 +1,96 @@
-//! `tcat [INPUT]`
-//!
-//! Decode time tags in .tags compressed binary format and output
-//! tab-separated tags. `tcat` is named in analogy to programs like `zcat`
-//! that output (cf. `cat`) the decompressed content of a file.
-//!
-//! Most likely, you want the shell one-liner
-//!
-//!     tcat mydata.tags.zst > mydata.tsv
-//!
-//! to convert the compressed binary format to tab-separated values for
-//! the widest data interopability.
+use argh::FromArgs;
+use anyhow::{bail, Result};
+use either::{Left, Right};
+use std::fs::{self, File};
+use std::io::{stdin, stdout, BufReader, Write};
 
 use tagtools::{ser, de};
 
-use anyhow::{anyhow, Result};
-use std::boxed::Box;
-use std::env;
-use std::fs::File;
-use std::io::{stdin, stdout, BufReader, Read, Write};
+const GIT_VERSION: &str = git_version::git_version!();
+
+#[derive(Debug, FromArgs, Clone)]
+/// Decode time tags in .tags.zst compressed binary format and print
+/// tab-separated tags to standard output. tcat is named in analogy
+/// to programs like zcat(1) that output the decompressed content of
+/// file(s).
+pub struct CliArgs {
+    /// print version information
+    #[argh(switch, short = 'v')]
+    pub version: bool,
+    /// with no input or when input is '-', read from standard input
+    #[argh(positional)]
+    pub input: Vec<String>,
+}
 
 fn main() -> Result<()> {
-    let args = env::args().collect::<Vec<_>>();
-    let stdin = stdin();
-    let stdout = stdout();
-    let mut rdr: BufReader<Box<dyn Read>>;
-    let mut wtr: csv::Writer<Box<dyn Write>>;
-    match args.len() - 1 {
-        0 => {
-            let iptr = Box::new(stdin.lock());
-            rdr = BufReader::new(iptr);
-
-            let optr = Box::new(stdout.lock());
-            wtr = csv::WriterBuilder::new()
-                .has_headers(false)
-                .delimiter(b'\t')
-                .from_writer(optr);
-        },
-        1 => {
-            let file = &args[1];
-            rdr = BufReader::new(Box::new(File::open(file)?));
-
-            let optr = Box::new(stdout.lock());
-            wtr = csv::WriterBuilder::new()
-                .has_headers(false)
-                .delimiter(b'\t')
-                .from_writer(optr);
-        },
-        _ => return Err(anyhow!("Wrong number of arguments")),
+    let args: CliArgs = argh::from_env();
+    if args.version {
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
+        writeln!(
+            stdout,
+            concat!(
+                env!("CARGO_BIN_NAME"),
+                " ",
+                "{}",
+            ),
+            GIT_VERSION,
+        )?;
+        return Ok(())
     }
-    let tags = de::tags(&mut rdr)?;
-    ser::tsv(&mut wtr, &tags)?;
+
+    // Collect inputs
+    let mut inputs = Vec::new();
+    if args.input.len() == 0 {
+        inputs.push(Left(()));
+    } else {
+        let mut contains_stdin = false;
+        for i in args.input {
+            if i == "-" {
+                if contains_stdin {
+                    panic!("cannot specify '-' for stdin twice");
+                } else {
+                    contains_stdin = true;
+                    inputs.push(Left(()));
+                }
+            } else {
+                match fs::metadata(&i) {
+                    Ok(m) => {
+                        if m.is_file() {
+                            inputs.push(Right(i));
+                        } else {
+                            bail!("{} is not a file", &i);
+                        }
+                    },
+                    Err(e) => bail!(e),
+                }
+            }
+        }
+    }
+
+    let stdout = stdout();
+    let stdout = stdout.lock();
+    let mut wtr = csv::WriterBuilder::new()
+        .has_headers(false)
+        .delimiter(b'\t')
+        .from_writer(stdout);
+
+    for i in inputs {
+        match i {
+            Left(()) => {
+                let stdin = stdin();
+                let stdin = stdin.lock();
+                let rdr = BufReader::new(stdin);
+                let tags = de::tags(rdr).expect("Cannot deserialize tags from file");
+                ser::tsv(&mut wtr, &tags)?;
+            },
+            Right(path) => {
+                let f = File::open(path)?;
+                let rdr = BufReader::new(f);
+                let tags = de::tags(rdr).expect("Cannot deserialize tags from file");
+                ser::tsv(&mut wtr, &tags)?;
+            },
+        }
+    }
     Ok(())
 }
--- tagtools/src/bin/txt2tags.rs
+++ tagtools/src/bin/txt2tags.rs
@@ -1,53 +1,112 @@
-//! `txt2tags [INPUT]`
-//!
-//! Encode time tags in `.tags.zst` compressed binary format from
-//! tab-separated tags.
-//!
-//! Most likely, you want the shell one-liner
-//!
-//!     txt2tags mydata.txt > mydata.tags.zst
-//!
-//! to convert tab-separated data to the compressed binary format
+use argh::FromArgs;
+use anyhow::{bail, Result};
+use either::{Left, Right};
+use std::fs::{self, File};
+use std::io::{stdin, stdout, BufReader, Write, BufWriter};
 
 use tagtools::{ser, de};
 
-use anyhow::{anyhow, Result};
-use std::boxed::Box;
-use std::env;
-use std::fs::File;
-use std::io::{stdin, stdout, Read, Write};
+const GIT_VERSION: &str = git_version::git_version!();
+
+#[derive(Debug, FromArgs, Clone)]
+/// Encode time tags stored as tab-separated values to the
+/// .tags.zst compressed binary format. Note: on Windows
+/// -o must be specified as the encoded data is not valid
+/// UTF-8 and thus cannot be written to stdout (a Rust stdlib
+/// limitation)
+pub struct CliArgs {
+    /// print version information
+    #[argh(switch, short = 'v')]
+    pub version: bool,
+    /// file to write output to (writes to standard output by default)
+    #[argh(option, short = 'o')]
+    pub out: Option<String>,
+    /// with no input or when input is '-', read from standard input
+    #[argh(positional)]
+    pub input: Vec<String>,
+}
 
 fn main() -> Result<()> {
-    let args = env::args().collect::<Vec<_>>();
-    let stdin = stdin();
-    let stdout = stdout();
-    let mut rdr: csv::Reader<Box<dyn Read>>;
-    let mut wtr: Box<dyn Write>;
-    match args.len() - 1 {
-        0 => {
-            let iptr = Box::new(stdin.lock());
-            rdr = csv::ReaderBuilder::new()
-                .has_headers(false)
-                .delimiter(b'\t')
-                .from_reader(iptr);
-
-            let optr = Box::new(stdout.lock());
-            wtr = optr;
-        },
-        1 => {
-            let file = &args[1];
-            let iptr = Box::new(File::open(file)?);
-            rdr = csv::ReaderBuilder::new()
-                .has_headers(false)
-                .delimiter(b'\t')
-                .from_reader(iptr);
-
-            let optr = Box::new(stdout.lock());
-            wtr = optr;
-        },
-        _ => return Err(anyhow!("Wrong number of arguments")),
+    let args: CliArgs = argh::from_env();
+    if args.version {
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
+        writeln!(
+            stdout,
+            concat!(
+                env!("CARGO_BIN_NAME"),
+                " ",
+                "{}",
+            ),
+            GIT_VERSION,
+        )?;
+        return Ok(())
     }
-    let tags = de::tsv(&mut rdr)?;
-    ser::tags(&mut wtr, &tags)?;
+
+    // Collect inputs
+    let mut inputs = Vec::new();
+    if args.input.len() == 0 {
+        inputs.push(Left(()));
+    } else {
+        let mut contains_stdin = false;
+        for i in args.input {
+            if i == "-" {
+                if contains_stdin {
+                    panic!("cannot specify '-' for stdin twice");
+                } else {
+                    contains_stdin = true;
+                    inputs.push(Left(()));
+                }
+            } else {
+                match fs::metadata(&i) {
+                    Ok(m) => {
+                        if m.is_file() {
+                            inputs.push(Right(i));
+                        } else {
+                            bail!("{} is not a file", &i);
+                        }
+                    },
+                    Err(e) => bail!(e),
+                }
+            }
+        }
+    }
+
+    let stdout = stdout();
+    let mut wtr: Box<dyn Write> = match args.out {
+        None => {
+            Box::new(stdout.lock())
+        },
+        Some(p) => {
+            let f = File::create(p)?;
+            Box::new(BufWriter::new(f))
+        },
+    };
+
+    for i in inputs {
+        match i {
+            Left(()) => {
+                let stdin = stdin();
+                let stdin = stdin.lock();
+                let brdr = BufReader::new(stdin);
+                let mut rdr = csv::ReaderBuilder::new()
+                    .has_headers(false)
+                    .delimiter(b'\t')
+                    .from_reader(brdr);
+                let tags = de::tsv(&mut rdr).expect("Cannot deserialize tags from file");
+                ser::tags(&mut wtr, &tags)?;
+            },
+            Right(path) => {
+                let f = File::open(path)?;
+                let brdr = BufReader::new(f);
+                let mut rdr = csv::ReaderBuilder::new()
+                    .has_headers(false)
+                    .delimiter(b'\t')
+                    .from_reader(brdr);
+                let tags = de::tsv(&mut rdr).expect("Cannot deserialize tags from file");
+                ser::tags(&mut wtr, &tags)?;
+            },
+        }
+    }
     Ok(())
 }
